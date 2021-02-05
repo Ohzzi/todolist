44928f859185d624b34a558d021e2e502da4e275
Oh <clover7even@naver.com>
2021-02-05 14:25:57 +0900

    feat: 화면 기본 구조 생성

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,24 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { createGlobalStyle } from 'styled-components';
+import ContentsBox from './components/ContentsBox';
+import ContentsLayout from './components/ContentsLayout';
+
+const GlobalStyle = createGlobalStyle`
+  body {
+    background: #e9ecef;
+    padding: 0;
+    margin: 0;
+  }
+`;
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <GlobalStyle />
+      <ContentsLayout>
+        <ContentsBox> 컨텐츠 박스 </ContentsBox>
+        <ContentsBox> 컨텐츠 박스 </ContentsBox>
+      </ContentsLayout>
+    </>
   );
 }
 
