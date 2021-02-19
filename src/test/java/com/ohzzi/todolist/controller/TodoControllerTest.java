bc506849c8426fcb3f31c4aea4f628896f8dccea
Ohzzi <clover7even@naver.com>
2021-02-19 17:55:56 +0900

    test: TodoControllerTest에 스프링 시큐리티 설정

--- src/test/java/com/ohzzi/todolist/controller/TodoControllerTest.java
+++ src/test/java/com/ohzzi/todolist/controller/TodoControllerTest.java
@@ -1,21 +1,35 @@
 package com.ohzzi.todolist.controller;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.ohzzi.todolist.controller.dto.TodoSaveRequestDto;
 import com.ohzzi.todolist.controller.dto.TodoUpdateRequestDto;
 import com.ohzzi.todolist.domain.todo.Todo;
 import com.ohzzi.todolist.domain.todo.TodoRepository;
+import com.ohzzi.todolist.domain.user.Role;
+import com.ohzzi.todolist.domain.user.User;
+import com.ohzzi.todolist.domain.user.UserRepository;
 import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.boot.web.server.LocalServerPort;
 import org.springframework.http.*;
+import org.springframework.security.test.context.support.WithMockUser;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
 
 import java.time.LocalDate;
+import java.util.Arrays;
 import java.util.List;
 
 import static org.assertj.core.api.Assertions.*;
+import static org.springframework.security.test.web.servlet.setup.SecurityMockMvcConfigurers.springSecurity;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 class TodoControllerTest {
@@ -26,61 +40,102 @@ class TodoControllerTest {
     private TestRestTemplate testRestTemplate;
     @Autowired
     private TodoRepository todoRepository;
+    @Autowired
+    private UserRepository userRepository;
+
+    @Autowired
+    private WebApplicationContext context;
+
+    private MockMvc mvc;
+
+    @BeforeEach
+    public void setup() {
+        mvc = MockMvcBuilders
+                .webAppContextSetup(context)
+                .apply(springSecurity())
+                .build();
+    }
 
     @AfterEach
     void afterEach() {
         todoRepository.deleteAll();
+        userRepository.deleteAll();
     }
 
     @Test
-    void todo_등록() {
+    @WithMockUser
+    void todo_등록() throws Exception{
         // given
+        String userEmail = "user@google.com";
+        String userName = "유저";
+        User user = User.builder()
+                .email(userEmail)
+                .name(userName)
+                .role(Role.USER)
+                .build();
+        userRepository.save(user);
+
         String content = "content";
         LocalDate date = LocalDate.now();
         TodoSaveRequestDto requestDto = TodoSaveRequestDto.builder()
                 .content(content)
                 .date(date)
                 .isImportant(true)
+                .user(user)
                 .build();
 
         String url = "http://localhost:" + port + "/api/todo";
 
         // when
-        ResponseEntity<Long> responseEntity = testRestTemplate.postForEntity(url, requestDto, Long.class);
+        mvc.perform(post(url)
+                .content(new ObjectMapper().writeValueAsString(requestDto))
+                .contentType(MediaType.APPLICATION_JSON_VALUE)
+                .accept(MediaType.APPLICATION_JSON_VALUE))
+                .andExpect(status().isOk());
 
         // then
-        assertThat(responseEntity.getStatusCode()).isEqualTo(HttpStatus.OK);
-        assertThat(responseEntity.getBody()).isGreaterThan(0L);
-
         List<Todo> all = todoRepository.findAll();
         assertThat(all.get(0).getContent()).isEqualTo(content);
         assertThat(all.get(0).getDate()).isEqualTo(date);
         assertThat(all.get(0).isImportant()).isEqualTo(true);
         assertThat(all.get(0).isActivated()).isEqualTo(false);
+        assertThat(all.get(0).getUser().getName()).isEqualTo(userName);
+        assertThat(all.get(0).getUser().getEmail()).isEqualTo(userEmail);
     }
 
     @Test
-    void Todo_조회() {
+    @WithMockUser()
+    void Todo_조회() throws Exception{
         // given
+        String userEmail = "user@google.com";
+        String userName = "유저";
+        User user = User.builder()
+                .email(userEmail)
+                .name(userName)
+                .role(Role.USER)
+                .build();
+        userRepository.save(user);
+
         String content = "content";
         LocalDate date = LocalDate.now();
         Todo savedTodo = todoRepository.save(Todo.builder()
                 .content(content)
                 .date(date)
                 .isImportant(true)
+                .user(user)
                 .build());
 
         long id = savedTodo.getId();
 
-        // when
         String url = "http://localhost:" + port + "/api/todo/" + id;
 
-        ResponseEntity<String> responseEntity = testRestTemplate.getForEntity(url, String.class);
+        // when
+        MvcResult result = mvc.perform(get(url)
+                .contentType(MediaType.APPLICATION_JSON_VALUE))
+                .andExpect(status().isOk())
+                .andReturn();
 
-        assertThat(responseEntity.getStatusCode()).isEqualTo(HttpStatus.OK);
-        assertThat(responseEntity.getBody()).isNotNull();
-
-        String body = responseEntity.getBody();
+        String body = result.getResponse().getContentAsString();
 
         assertThat(body).contains(content);
         assertThat(body).contains(date.toString());
@@ -89,14 +144,25 @@ class TodoControllerTest {
     }
 
     @Test
-    void Todo_수정() {
+    @WithMockUser
+    void Todo_수정() throws Exception {
         // given
+        String userEmail = "user@google.com";
+        String userName = "유저";
+        User user = User.builder()
+                .email(userEmail)
+                .name(userName)
+                .role(Role.USER)
+                .build();
+        userRepository.save(user);
+
         String content = "content";
         LocalDate date = LocalDate.now();
         Todo savedTodo = todoRepository.save(Todo.builder()
                 .content(content)
                 .date(date)
                 .isImportant(true)
+                .user(user)
                 .build());
 
         Long id = savedTodo.getId();
@@ -108,44 +174,54 @@ class TodoControllerTest {
                 .isActivated(true)
                 .build();
 
-        HttpEntity<TodoUpdateRequestDto> requestEntity = new HttpEntity<>(requestDto);
-
         String url = "http://localhost:" + port + "/api/todo/" + id;
 
         // when
-        ResponseEntity<Long> responseEntity
-                = testRestTemplate.exchange(url, HttpMethod.PUT, requestEntity, Long.class);
-
-        assertThat(responseEntity.getStatusCode()).isEqualTo(HttpStatus.OK);
-        assertThat(responseEntity.getBody()).isGreaterThan(0L);
+        mvc.perform(put(url)
+                .content(new ObjectMapper().writeValueAsString(requestDto))
+                .contentType(MediaType.APPLICATION_JSON_VALUE)
+                .accept(MediaType.APPLICATION_JSON_VALUE))
+                .andExpect(status().isOk());
 
         Todo updatedTodo = todoRepository.findById(id).orElseGet(() -> fail(""));
 
         assertThat(updatedTodo.getContent()).isEqualTo(expectedContent);
         assertThat(updatedTodo.isImportant()).isEqualTo(false);
         assertThat(updatedTodo.isActivated()).isEqualTo(true);
-
+        assertThat(updatedTodo.getUser().getName()).isEqualTo(userName);
+        assertThat(updatedTodo.getUser().getEmail()).isEqualTo(userEmail);
     }
 
     @Test
-    void Todo_삭제() {
+    @WithMockUser
+    void Todo_삭제() throws Exception{
         // given
+        String userEmail = "user@google.com";
+        String userName = "유저";
+        User user = User.builder()
+                .email(userEmail)
+                .name(userName)
+                .role(Role.USER)
+                .build();
+        userRepository.save(user);
+
         String content = "content";
         LocalDate date = LocalDate.now();
         Todo savedTodo = todoRepository.save(Todo.builder()
                 .content(content)
                 .date(date)
                 .isImportant(true)
+                .user(user)
                 .build());
 
         long id = savedTodo.getId();
         String url = "http://localhost:" + port + "/api/todo/" + id;
 
         // when
-        testRestTemplate.delete(url);
+        mvc.perform(delete(url))
+                .andExpect(status().isOk());
 
         // then
-
         List<Todo> all = todoRepository.findAll();
         assertThat(all.size()).isEqualTo(0);
     }
