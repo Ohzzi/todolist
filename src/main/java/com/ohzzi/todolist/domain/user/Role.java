c90960ffe42ab97894806843be1544327e9a41e2
Ohzzi <clover7even@naver.com>
2021-02-19 14:07:47 +0900

    fix: 불필요한 Role 제거

--- src/main/java/com/ohzzi/todolist/domain/user/Role.java
+++ src/main/java/com/ohzzi/todolist/domain/user/Role.java
@@ -7,8 +7,7 @@ import lombok.RequiredArgsConstructor;
 @RequiredArgsConstructor
 public enum Role {
 
-    USER("ROLE_USER", "일반 사용자"),
-    GUEST("ROLE_GUEST", "손님");
+    USER("ROLE_USER", "일반 사용자");
 
     private final String key;
     private final String title;
